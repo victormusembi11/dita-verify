02f0d43987e023976427f940a6b637864590ed4e
victormusembi11 <victor.musembi11@gmail.com>
2026-04-07 14:56:15 +0300

    chore: update version to 1.0.4 and refactor baseUrl getter in config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,11 @@
 /**
  * Configuration for the Daystar Verify package.
+ * This includes settings such as the base URL for the Daystar Portal API.
+ * The base URL can be set via the environment variable `DAYSTAR_PORTAL_URL`.
+ * If the environment variable is not set, it defaults to an empty string.
  */
 export const config = {
-  baseUrl: process.env.DAYSTAR_PORTAL_URL ?? "",
+  get baseUrl() {
+    return process.env.DAYSTAR_PORTAL_URL ?? "";
+  },
 };
